e804ea032f212e52e339711541804b3edd25ac87
mrAntonio10 <111607077+mrAntonio10@users.noreply.github.com>
2022-08-20 23:18:24 -0400

    Delete Figuras directory

--- Figuras/src/interfaces/IDosDecimales.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import java.text.DecimalFormat;
-
-public interface IDosDecimales {
-        double dosDecimales(double n);
-}
--- Figuras/src/interfaces/IPositivo.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface IPositivo {
-
-
-    double esPositivo(double n);
-}
